abb29b0c5514d16eb61138ae238b30d21efc2230
PeeniseGänkster! <50110197+hunthell24@users.noreply.github.com>
2020-11-30 14:10:09 +0200

    Add files via upload

---
+++ chuckapi/chuckapi/js/app.js
@@ -0,0 +1,26 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(event){
+
+    let url = `http://api.icndb.com/jokes/random`;
+
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        let output = '';
+        output += `${data.value.joke}`;
+        document.querySelector('.jokes').innerHTML = output;
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    let icon = '';
+        output += `${icon.value.joke}`;
+
+    event.preventDefault();
+}
+
